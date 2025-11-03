eadf0e03ba8e7c2da7f84376b32b59c93b39622a
alfredoyu <109763166+alfredoyu@users.noreply.github.com>
2025-11-03 15:24:50 +0800

    Update hero background with landscape image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,7 +56,7 @@ const Home: React.FC = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: 'url(/images/hero-bg.jpeg)'
+            backgroundImage: 'url(/images/e5547271aa7717b9c3e97c5e0dcbe0d5.jpg)'
           }}
         />
         {/* Overlay for better text readability */}
